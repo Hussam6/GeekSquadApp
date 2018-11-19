9b5a6a1a4848cd988f20af2254722b95d5eb69f3
Jack Webster <websjack@sheridancollege.ca>
2018-11-19 13:01:37 -0500

    added a comment just to check if i can commit/push

--- Geek Squad/App.xaml.cs
+++ Geek Squad/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+//comment for testing version control
 
 namespace Geek_Squad
 {
