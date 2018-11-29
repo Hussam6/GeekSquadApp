a05c79ffed4084138dc3cae433f31f6ad9c1c7a0
Hussam Mufti <muftih@shernet.sheridancollege.ca>
2018-11-28 23:36:04 -0500

    Configured hyperlink button in MainPage

--- Geek Squad/MainPage.xaml.cs
+++ Geek Squad/MainPage.xaml.cs
@@ -34,7 +34,7 @@ namespace Geek_Squad
 
         private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(NewUser));
+            this.Frame.Navigate(typeof(CreateUser));
         }
     }
 }
